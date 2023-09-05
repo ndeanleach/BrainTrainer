8209968b3a98706b833b70830bb1ca6b42994363
deannate00 <deannate00@gmail.com>
2023-09-05 11:02:43 -0400

    Upload

--- BrainTrainer/ContentView.swift
+++ BrainTrainer/ContentView.swift
@@ -7,13 +7,15 @@
 
 import SwiftUI
 
+// testing
+
 struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, test!")
         }
         .padding()
     }
