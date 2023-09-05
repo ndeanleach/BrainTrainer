6f7780837c2d71f141b30d52f98b42c67947da24
deannate00 <deannate00@gmail.com>
2023-09-05 11:54:13 -0400

    Partial Logic

--- BrainTrainer/ContentView.swift
+++ BrainTrainer/ContentView.swift
@@ -7,17 +7,93 @@
 
 import SwiftUI
 
-// testing
-
 struct ContentView: View {
+    
+    @State private var appChoice = "📄"
+    @State private var playerChoice = "🪨"
+    
+    @State private var winLose = Bool.random()
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    
+    @State private var playerScore = 0
+    
+    let choices: [String: String] = ["🪨": "✂️", "✂️": "📄", "📄": "🪨"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, test!")
+        NavigationStack {
+            ZStack{
+                LinearGradient(
+                    gradient: Gradient(colors: [Color.cyan, Color.gray]),
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+                .edgesIgnoringSafeArea(.all)
+                VStack{
+                    Text(winLose == true ? "Win" : "Lose")
+                        .font(.largeTitle)
+                        .bold()
+                    Text("Opponent: \(appChoice)")
+                        .font(.title)
+                        .padding(.bottom, 45)
+                    HStack{
+                        ForEach(Array(choices.keys), id: \.self){ choice in
+                            Button("\(choice)"){
+                                playerChoice = choice
+                                check()
+                            }
+                            .font(.largeTitle)
+                        }
+                    }
+                    Text("\(playerScore)")
+                        .padding(45)
+                        .font(.largeTitle)
+                }
+            }
+            .navigationTitle("Brain Trainer")
+            .alert(isPresented: $showingAlert){
+                Alert(
+                    title: Text("\(alertTitle)"),
+                    message: Text("\(alertMessage)"),
+                    primaryButton: .default(Text("OK")){
+                        appChoice =  Array(choices.values).randomElement() ?? ""
+                        winLose = Bool.random()
+                        showingAlert = false
+                    },
+                    secondaryButton: .cancel()
+                )
+            }
         }
-        .padding()
+    }
+    
+    func check(){
+        if winLose == true {
+            if let appResult = choices[playerChoice], appResult == appChoice {
+                alertTitle = "Correct"
+                alertMessage = "+1 point"
+                playerScore += 1
+                showingAlert = true
+            } else {
+                alertTitle = "Incorrect"
+                alertMessage = "-1 point"
+                playerScore -= 1
+                showingAlert = true
+            }
+        } else {
+            if let appResult = choices[playerChoice], appResult != appChoice{
+                alertTitle = "Correct"
+                alertMessage = "+1 point"
+                playerScore += 1
+                showingAlert = true
+            } else {
+                alertTitle = "Incorrect"
+                alertMessage = "-1 point"
+                playerScore -= 1
+                showingAlert = true
+            }
+        }
     }
 }
 
