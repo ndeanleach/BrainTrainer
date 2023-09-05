2a173defc0630149bf4f3d60b3a80456dfb2e28f
deannate00 <deannate00@gmail.com>
2023-09-05 11:54:13 -0400

    Partial Logic

--- BrainTrainer/ContentView.swift
+++ BrainTrainer/ContentView.swift
@@ -7,17 +7,73 @@
 
 import SwiftUI
 
-// testing
+// testing upload
 
 struct ContentView: View {
+    
+    @State private var appChoice = "📄"
+    @State private var winLose = Bool.random()
+    @State private var playerChoice = "🪨"
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    @State private var playerScore = 0
+    
+    var optionList = ["🪨", "📄", "✂️"].shuffled()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, test!")
+        NavigationStack {
+            VStack{
+                Text(winLose == true ? "Win" : "Lose")
+                    .font(.largeTitle)
+                    .bold()
+                Text("Opponent: \(appChoice)")
+                    .font(.title)
+                    .padding(.bottom, 45)
+                HStack{
+                    ForEach(optionList, id: \.self){
+                        Button("\($0)"){
+                            check()
+                        }
+                        .font(.largeTitle)
+                    }
+                }
+                Text("\(playerScore)")
+                    .padding(45)
+                    .font(.largeTitle)
+            }
+            .navigationTitle("Brain Trainer")
         }
         .padding()
+    }
+    
+    func check(){
+        if winLose == true{
+            if playerChoice == "🪨" && appChoice == "✂️"{
+                // add alert message
+                playerScore += 1
+            } else if playerChoice == "✂️" && appChoice == "📄"{
+                //add alert message
+                playerScore += 1
+            } else if playerChoice == "📄" && appChoice == "🪨"{
+                //add alert message
+                playerScore += 1
+            } else{
+                //add alert message
+            }
+        } else if winLose == false {
+            if playerChoice != "🪨" && appChoice == "✂️"{
+                // add alert message
+                playerScore += 1
+            } else if playerChoice != "✂️" && appChoice == "📄"{
+                //add alert message
+                playerScore += 1
+            } else if playerChoice != "📄" && appChoice == "🪨"{
+                //add alert message
+                playerScore += 1
+            } else{
+                //add alert message
+            }
+        }
     }
 }
 
