8383fb478f13af7e762adefb434bfb75aaeb2183
deannate00 <deannate00@gmail.com>
2023-09-05 10:59:17 -0400

    Initial Commit

---
+++ BrainTrainer/BrainTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainTrainerApp.swift
+//  BrainTrainer
+//
+//  Created by Nathan Leach on 9/5/23.
+//
+
+import SwiftUI
+
+@main
+struct BrainTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
